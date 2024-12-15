c704548b01ec0123aa6df0d56e1a5b72c8430db8
etemesi <caleb@techplain.io>
2024-12-15 18:13:23 +0300

    Add comments

--- rizumu/base_model.py
+++ rizumu/base_model.py
@@ -24,11 +24,12 @@ def _encoder_block(in_channels, out_channels):
 
 
 class BLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, layers=1, skip=False, *args, **kwargs):
+    def __init__(self, input_size, hidden_size, output_size, layers=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.conv1 = nn.Conv2d(input_size, input_size, kernel_size=1)
 
-        self.lstm = nn.LSTM(bidirectional=True, num_layers=layers, hidden_size=hidden_size, input_size=input_size,
+        self.lstm = nn.LSTM(bidirectional=True, num_layers=layers,
+                            hidden_size=hidden_size, input_size=input_size,
                             batch_first=True)
         self.linear = nn.Linear(2 * hidden_size, hidden_size)
         self.conv2 = nn.Conv2d(hidden_size, output_size, kernel_size=1)
@@ -39,6 +40,7 @@ class BLSTM(nn.Module):
         # reshape
         batch_size, channels, height, width = x.shape
         x = x.permute(0, 2, 3, 1).reshape(batch_size, height * width, channels)
+
         x = self.lstm(x)[0]
         x = self.linear(x)
 
@@ -118,15 +120,15 @@ class SourceSeparationModel(nn.Module):
         # build encoder
         #
         # n -> 1,64,128,256,512..2**n -> based on depth.
-        # but first one must start at 1, so that is a special
+        # but first one must start at input_channels, so that is a special
         # case
         for i in range(depth):
-            n = 2 ** (i + 5)
-            n_1 = 2 ** (i + 6)
+            input_size = 2 ** (i + 5)
+            output_size = 2 ** (i + 6)
             if i == 0:
-                self.encoders.append(SourceEncoder(input_size=input_channels, output_size=n_1))
+                self.encoders.append(SourceEncoder(input_size=input_channels, output_size=output_size))
             else:
-                self.encoders.append(SourceEncoder(input_size=n, output_size=n_1))
+                self.encoders.append(SourceEncoder(input_size=input_size, output_size=output_size))
 
         # Bi-LSTM Bridge
         #
@@ -139,15 +141,19 @@ class SourceSeparationModel(nn.Module):
         self.decoders = nn.ModuleList()
 
         for i in range(depth - 1):
-            n_1 = 2 ** ((5 + depth) - i)
-            n = 2 ** ((4 + depth) - i)
-            dec4 = SourceDecoder(n_1, n)
-            self.decoders.append(dec4)
+            # the reduction networks create smaller outputs from
+            # larger inputs
+            input_size = 2 ** ((5 + depth) - i)
+            output_size = 2 ** ((4 + depth) - i)
+            decoder = SourceDecoder(input_size, output_size)
+            self.decoders.append(decoder)
 
         # last layer does not do skip connections and hence why it is separate
-        self.final = nn.Sequential(nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
-                                   nn.Conv2d(32, output_channels, kernel_size=1),
-                                   nn.ReLU())
+        # Adding RELU does really nasty things to model performance
+        self.final = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
+            nn.Conv2d(32, output_channels, kernel_size=1),
+        )
 
     def forward(self, x):
         """
@@ -160,16 +166,16 @@ class SourceSeparationModel(nn.Module):
         Returns:
             torch.Tensor: Separated source spectrogram
         """
-        orig = x
         orig_shape = x.shape
 
         # Pad the network since the downsample
         # network will reduce it and will lose the odd network
-        # doing it to 16 because we encode 4 layers deep and each layer
+        # doing it to 2**depth because we encode depth layers deep and each layer
         # divides it by half.
         x = pad_to_multiple_of_n(x, 2 ** self.depth)
 
         encode_results = []
+
         for encoder in self.encoders:
             x = encoder(x)
             encode_results.append(x)
@@ -181,12 +187,12 @@ class SourceSeparationModel(nn.Module):
         for decoder in self.decoders:
             x = decoder(x, encode_results.pop(-1))
 
-        #
+
         x = self.final(x)
 
         # remove the padding by slicing
         x = x[:, :, :orig_shape[-2], :orig_shape[-1]]
-        # mask the original piece.
+
         return x
 
 
@@ -198,10 +204,7 @@ if __name__ == "__main__":
     stft = torch.stft(audio, n_fft=2048, return_complex=True, window=torch.hann_window(2048))
     stft = stft.unsqueeze(0).to("mps")
 
-    model = SourceSeparationModel(input_channels=1, output_channels=1, depth=3, hidden_size=512, lstm_layers=1).to(
+    model = SourceSeparationModel(input_channels=1, output_channels=1, depth=4, hidden_size=512, lstm_layers=2).to(
         "mps")
     s = torch.abs(stft).to("mps")
-    out = model(s)
-    print(s.shape)
-    c = torchinfo.summary(model, input_data=out, device="mps", depth=4)
-    print(out.shape)
+    c = torchinfo.summary(model, input_data=s, device="mps", depth=4)
